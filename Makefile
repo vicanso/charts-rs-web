aeaaf91b9105056f0c7d5d8a9d1c644670103d02
vicanso <tree.xie@outlook.com>
2023-09-24 12:06:30 +0800

    refactor: support more themes

--- Makefile
+++ Makefile
@@ -12,5 +12,9 @@ udeps:
 build-web:
 	cd web && yarn && yarn build && cd .. && rm -rf dist && mv web/dist .
 
+# 如果要使用需注释 profile.release 中的 strip
+bloat:
+	cargo bloat --release --crates
+
 release:
 	cargo build --release
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -132,7 +132,29 @@ const themeOptions = [
     value: "ant",
     label: "Ant",
   },
+  {
+    value: "vintage",
+    label: "Vintage",
+  },
+  {
+    value: "walden",
+    label: "Walden",
+  },
+  {
+    value: "westeros",
+    label: "Westeros",
+  },
+  {
+    value: "chalk",
+    label: "Chalk",
+  },
+  {
+    value: "shine",
+    label: "Shine",
+  },
 ];
+
+
 const formatOptions = [
   {
     value: "svg",
@@ -762,7 +784,60 @@ const chartDefaultOptions: Record<string, unknown> = {
       Object.assign({}, defaultOption, {
         width: 400,
         height: 300,
-        x: 215,
+        x: 10,
+        y: 320,
+        type: "scatter",
+        title_text: "Height and weight",
+        title_align: "left",
+        sub_title_text: "Data from: Heinz 2003",
+        sub_title_align: "left",
+        legend_align: "right",
+        margin: {
+          left: 5,
+          top: 5,
+          right: 20,
+          bottom: 5,
+        },
+        y_axis_configs: [
+          {
+            axis_min: 40,
+            axis_max: 100,
+            axis_formatter: "{c} kg",
+          },
+        ],
+        x_axis_config: {
+          axis_min: 140,
+          axis_max: 200,
+          axis_formatter: "{c} cm",
+        },
+        series_list: [
+          {
+            name: "Female",
+            data: [
+              161.2, 51.6, 167.5, 59.0, 159.5, 49.2, 157.0, 63.0, 155.8, 53.6,
+              170.0, 59.0, 159.1, 47.6, 166.0, 69.8, 176.2, 66.8, 160.2, 75.2,
+              172.5, 55.2, 170.9, 54.2, 172.9, 62.5, 153.4, 42.0, 160.0, 50.0,
+              147.2, 49.8, 168.2, 49.2, 175.0, 73.2, 157.0, 47.8, 167.6, 68.8,
+              159.5, 50.6, 175.0, 82.5, 166.8, 57.2, 176.5, 87.8, 170.2, 72.8,
+            ],
+          },
+          {
+            name: "Male",
+            data: [
+              174.0, 65.6, 175.3, 71.8, 193.5, 80.7, 186.5, 72.6, 187.2, 78.8,
+              181.5, 74.8, 184.0, 86.4, 184.5, 78.4, 175.0, 62.0, 184.0, 81.6,
+              180.0, 76.6, 177.8, 83.6, 192.0, 90.0, 176.0, 74.6, 174.0, 71.0,
+              184.0, 79.6, 192.7, 93.8, 171.5, 70.0, 173.0, 72.4, 176.0, 85.9,
+              176.0, 78.8, 180.5, 77.8, 172.7, 66.2, 176.0, 86.4, 173.5, 81.8,
+            ],
+          },
+        ],
+        series_symbol_sizes: [6, 6],
+      }),
+      Object.assign({}, defaultOption, {
+        width: 400,
+        height: 300,
+        x: 420,
         y: 320,
         type: "pie",
         title_text: "Nightingale Chart",
